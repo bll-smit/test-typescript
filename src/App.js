b5bf72cef1119998d1f94019ca1c379e26f38171
Smit Vora <>
2019-12-19 15:18:58 +0530

    added link in comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import React from "react";
 import "./App.css";
 import GrandParent from "./GrandParent/GrandParent";
-
+/**
+ * https://medium.com/@martin_hotell/build-100-type-safe-react-apps-in-vanilla-javascript-bd29a8364078
+ */
 const App = () => {
   return (
     <div className="App">
